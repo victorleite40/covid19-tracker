4cd9686c5a504fe5448a4d2993300216368d87b0
victorleite40 <victorsalvetti@hotmail.com>
2020-04-23 20:13:22 -0300

    Updates

---
+++ precache-manifest.b26ff96a319459722f0a358c15f85847.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02354fcde15a50c9801fc7774b05e22e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "55526070c0f9deda0a3c",
+    "url": "/static/css/main.e11a60f6.chunk.css"
+  },
+  {
+    "revision": "a904de5f730af6055f5e",
+    "url": "/static/js/2.63db199e.chunk.js"
+  },
+  {
+    "revision": "c7a7e79b10ab645cab64be742e1dd9e5",
+    "url": "/static/js/2.63db199e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55526070c0f9deda0a3c",
+    "url": "/static/js/main.3f5367ff.chunk.js"
+  },
+  {
+    "revision": "055e94f6602794288cc3",
+    "url": "/static/js/runtime-main.1f1f19a3.js"
+  },
+  {
+    "revision": "d0efc7571f8966a14e4ac3e18813b5ef",
+    "url": "/static/media/arrow-down.d0efc757.svg"
+  },
+  {
+    "revision": "3f31c18463b01ef0defceed6e3d9df0c",
+    "url": "/static/media/arrow-up.3f31c184.svg"
+  }
+]);
