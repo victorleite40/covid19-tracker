3ca2e7864d63454b824054b6c13263142823db87
victorleite40 <victorsalvetti@hotmail.com>
2020-04-30 12:24:24 -0300

    Updated page

--- src/pages/Local/index.js
+++ src/pages/Local/index.js
@@ -5,34 +5,25 @@ import apiCovid from '../../services/api';
 import Header from '../../components/Header/Header'
 import Ranking from '../../components/Ranking/Ranking'
 
-import Propragacao from '../../components/Propragacao'
+import SpreadChart from '../../components/SpreadChart'
 import NowActive from '../../components/NowActive'
 import StatsChart from '../../components/StatsChart/StatsChart'
 import DailyData from '../../components/DailyData'
+import MostCases from '../../components/MostCases'
 
 import './styles.css';
 
 export default function Main() {
+    const { country } = useParams();
+
     const [summary, setSummary] = useState([]);
     const [previousSummary, setPreviousSummary] = useState([]);
 
     /**
-     * CHARTS DATA
-     */
-    const [date, setDate] = useState([]);
-    const [confirmed, setConfirmed] = useState([]);
-    const [fatal, setFatal] = useState([]);
-    const [activeChart, setActiveChart] = useState([]);
-
-    const [dailyRecovered, setDailyRecovered] = useState([]);
-    const [dailyFatal, setDailyFatal] = useState([]);
-    
-    const { country } = useParams();
-
+    * GET SUMMARY
+    */
     useEffect(() =>{
-        /**
-         * GET SUMMARY
-         */
+        
         async function loadCountrySummary() {
             const response = await apiCovid.get(`/total/country/${country}`)
 
@@ -42,66 +33,14 @@ export default function Main() {
             setSummary(response.data[response.data.length-lastRep])
             setPreviousSummary(response.data[(response.data.length-lastRep)-1])
         }
-
-        /**
-         * GET TOTAL DATA
-         */
-        async function loadCountryTotalData() {
-            const response = await apiCovid.get(`/total/country/${country}`)
-            
-            const labelDate = []
-            const confirmedData = []
-            const fatalData = []
-            const activeChartData = []
-
-            const dailyRecoveredData = []
-            const dailyFatalData = []
-
-            for (let i=0; i<response.data.length; i+=5) {
-                // Ignores if there are 0 cases confirmed
-                if (response.data[i].Confirmed!==0) {
-                    // Spread Chart
-                    labelDate.push(response.data[i].Date.slice(0, 10));
-                    confirmedData.push(response.data[i].Confirmed);
-                    fatalData.push(response.data[i].Deaths);
-                    
-                    // Now Active Chart
-                    activeChartData.push(response.data[i].Confirmed-response.data[i].Recovered-response.data[i].Deaths);
-
-                    // Daily Data Chart
-                    dailyRecoveredData.push(response.data[i].Recovered);
-                    dailyFatalData.push(response.data[i].Deaths);
-                } else {
-                    i--;
-                }
-            }
-
-            // Checks if the last report is actually valid
-            const lastRep = (response.data[response.data.length-1].Confirmed!==0) ? 1 : 2;
-
-            // Pushes the last report
-            labelDate.push(response.data[response.data.length-lastRep].Date.slice(0, 10));
-            confirmedData.push(response.data[response.data.length-lastRep].Confirmed);
-            fatalData.push(response.data[response.data.length-lastRep].Deaths);
-            activeChartData.push(response.data[response.data.length-lastRep].Confirmed-response.data[response.data.length-lastRep].Recovered-response.data[response.data.length-lastRep].Deaths);
-
-            dailyRecoveredData.push(response.data[response.data.length-lastRep].Recovered);
-            dailyFatalData.push(response.data[response.data.length-lastRep].Deaths);
-
-            setDate(labelDate)
-            setConfirmed(confirmedData)
-            setFatal(fatalData)
-            setActiveChart(activeChartData)
-
-            setDailyRecovered(dailyRecoveredData)
-            setDailyFatal(dailyFatalData)
-        }
-        loadCountryTotalData();
         loadCountrySummary();
     }, [country]);
 
-    const newActive = (summary.Confirmed - summary.Recovered - summary.Deaths) - (previousSummary.Confirmed - previousSummary.Recovered - previousSummary.Deaths)
-    const nowActive = (summary.Confirmed - summary.Recovered - summary.Deaths)
+    /**
+     * DATA HANDLE
+     */
+    let newActive = (summary.Confirmed - summary.Recovered - summary.Deaths) - (previousSummary.Confirmed - previousSummary.Recovered - previousSummary.Deaths)
+    let nowActive = (summary.Confirmed - summary.Recovered - summary.Deaths)
     
     let activeStats = (nowActive*100)/summary.Confirmed;
     let recoveredStats = (summary.Recovered*100)/summary.Confirmed;
@@ -114,7 +53,7 @@ export default function Main() {
         <>
         <Header />
         <div className="content">
-            <h1> {summary.Country + " Statistics"} </h1>
+            <h1> {summary.Country} </h1>
             <div className="dashboardLocal">
                 <div className="localSummary">
                     {/* <strong>Total Confirmed Cases</strong> <p> {summary.Confirmed} </p> */}
@@ -137,13 +76,14 @@ export default function Main() {
 
                 <div className="subGridLocal">
                     <StatsChart title={summary.Country} page={"Local"} activeStats={activeStats.toFixed(2)} recoveredStats={recoveredStats.toFixed(2)} fatalStats={fatalStats.toFixed(2)} />
+                    <MostCases />
                 </div>
 
-                <Propragacao labelDate={date} confirmedData={confirmed} fatalData={fatal} />
-                <NowActive labelDate={date} activeData={activeChart} />
+                <SpreadChart />
+                <NowActive />
 
                 <Ranking page={"Local"} />
-                <DailyData labelDate={date} recovered={dailyRecovered} fatal={dailyFatal} />
+                <DailyData />
             </div>
         </div>
         </>
